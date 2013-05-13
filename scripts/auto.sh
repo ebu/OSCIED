6e795ab9915a912fc7a04935e9cdbb46ebfc11b3
David Fischer <david.fischer.ch@gmail.com>
2013-05-13 17:46:27 +0200

    Merge tmpfs script into auto

--- scripts/auto.sh
+++ scripts/auto.sh
@@ -1,3 +1,7 @@
+# Simple helper to speed-up LXC containers
+sudo umount /var/lib/lxc
+sudo mount -t tmpfs -o size=3072M tmpfs /var/lib/lxc
+
 # This is a temporary utility to speed-up testing of charms
 lu-importUtils ../charms/
 sh juju-menu.sh overwrite
--- scripts/tmpfs.sh
+++
@@ -1,2 +0,0 @@
-# Simple helper to speed-up LXC containers
-sudo mount -t tmpfs -o size=2048M tmpfs /var/lib/lxc/
