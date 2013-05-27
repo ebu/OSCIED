3443db6bbedce7a1742efe5a5f24ede4ed0980ad
David Fischer <david.fischer.ch@gmail.com>
2013-05-27 18:33:34 +0200

    May fix issue #57

--- charms/lib/StorageHooks.py
+++ charms/lib/StorageHooks.py
@@ -92,9 +92,9 @@ class StorageHooks(CharmHooks):
         volume = volume or self.volume
         ips = ','.join(filter(None, [self.config.allowed_ips, self.local_config.allowed_ips]))
         self.info('Set volume %s allowed clients IP list to %s' % (volume, ips))
-        self.volume_do('set', volume=volume, options='auth.allow %s' % ips, fail=False)
+        self.volume_do('set', volume=volume, options='auth.allow "%s"' % ips, fail=False)
         auth_allow = self.volume_infos(volume=volume)['auth_allow']
-        if auth_allow != ips:
+        if auth_allow not in ips:
             raise ValueError('Volume %s auth.allow=%s (expected %s)' % (volume, ips, auth_allow))
         self.info(self.volume_infos(volume=volume))
 
