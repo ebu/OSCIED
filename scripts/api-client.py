60adcfba81b29c338ae9dc5941a3d6044bcc7b9b
David Fischer <david.fischer.ch@gmail.com>
2013-07-09 11:24:10 +0200

    Rename api-menu.py to api-client.py + implementing more methods

--- scripts/api-client.py
+++ scripts/api-client.py
@@ -28,21 +28,15 @@ from __future__ import print_function
 
 import requests
 from pyutils.pyutils import object2json
-#import glob, re, shutil, os
-#from os.path import basename, dirname, join, splitext
-#from common import (
-#    REPORT_TOOLS_PLANTUML_BINARY, DAVID_REPORT_PATH, DAVID_REPORT_BUILD_PATH,
-#    DAVID_REPORT_RELEASE_PATH, DAVID_REPORT_SOURCE_PATH, DAVID_REPORT_UML_PATH,
-#    DAVID_REPORT_COMMON_FILE, DAVID_REPORT_REFERENCES_FILE, DAVID_REPORT_LINKS_FILE,
-#    WIKI_BUILD_PATH, WIKI_SOURCE_PATH, xprint
-#)
-#from pyutils.pyutils import cmd, try_makedirs, try_remove
+
 
 def api_method(args):
-    arg3 = '/%s' % args.arg3 if args.arg3 != 'list' else ''
+    VOID_ARGS = ('list', 'add', 'deploy')
+    arg2 = '/%s' % args.arg2 if (hasattr(args, 'arg2') and args.arg2 not in VOID_ARGS) else ''
+    arg3 = '/%s' % args.arg3 if (hasattr(args, 'arg3') and args.arg3 not in VOID_ARGS) else ''
     env = '/environment/%s' % args.environment if hasattr(args, 'environment') else ''
     num_units = '/%s' % args.num_units if hasattr(args, 'num_units') else ''
-    return 'http://%s:%s/%s/%s%s%s%s' % (args.host, args.port, args.arg1, args.arg2, env, arg3, num_units)
+    return 'http://%s:%s/%s%s%s%s%s' % (args.host, args.port, args.arg1, arg2, env, arg3, num_units)
 
 if __name__ == '__main__':
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
@@ -57,10 +51,20 @@ if __name__ == '__main__':
 
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter, epilog='''TODO.''')
     subparsers = parser.add_subparsers(dest='arg1')
+    environment_parser = subparsers.add_parser('environment')
+    environment_subparsers = environment_parser.add_subparsers(dest='arg2')
+    environment_list_parser = environment_subparsers.add_parser('list', parents=[api])
+    environment_add_parser = environment_subparsers.add_parser('add', parents=[api])
+    environment_add_parser.add_argument('name', action='store')
+    environment_add_parser.add_argument('type', action='store')
+    environment_add_parser.add_argument('region', action='store')
+    environment_add_parser.add_argument('access_key', action='store')
+    environment_add_parser.add_argument('secret_key', action='store')
+    environment_add_parser.add_argument('control_bucket', action='store')
     transform_parser = subparsers.add_parser('transform')
     transform_subparsers = transform_parser.add_subparsers(dest='arg2')
     transform_unit_parser = transform_subparsers.add_parser('unit')
-    transform_unit_subparsers = transform_unit_parser.add_subparsers(dest='arg3')
+    transform_unit_subparsers = transform_unit_parser.add_subparsers(dest='action')
     transform_unit_list_parser = transform_unit_subparsers.add_parser('list', parents=[api, environment])
     transform_unit_count_parser = transform_unit_subparsers.add_parser('count', parents=[api, environment])
     transform_unit_deploy_parser = transform_unit_subparsers.add_parser('deploy', parents=[api, environment])
@@ -73,7 +77,16 @@ if __name__ == '__main__':
 
     print(api_method(args))
 
-    if args.arg1 == 'transform':
+    if args.arg1 == 'environment':
+        if args.arg2 == 'list':
+            print('Listing environments ...')
+            r = requests.get(api_method(args), auth=auth)
+            print(r.json())
+        if args.arg2 == 'add':
+            print('Adding a new environment %s as %s ...' % (args.name, username))
+            r = requests.post(api_method(args), data=object2json(args, False), auth=auth, headers=headers)
+            print(r.json())
+    elif args.arg1 == 'transform':
         if args.arg2 == 'unit':
             if args.arg3 == 'list':
                 print('Listing transform units of environment %s as %s ...' % (args.environment, username))
