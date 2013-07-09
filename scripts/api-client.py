ff2583f195bce0350f615338dec2c23452144a24
David Fischer <david.fischer.ch@gmail.com>
2013-07-09 15:51:05 +0200

    Implement more methods, api_method improved

--- scripts/api-client.py
+++ scripts/api-client.py
@@ -27,77 +27,96 @@
 from __future__ import print_function
 
 import requests
+from pprint import pprint
 from pyutils.pyutils import object2json
 
 
-def api_method(args):
-    VOID_ARGS = ('list', 'add', 'deploy')
-    arg2 = '/%s' % args.arg2 if (hasattr(args, 'arg2') and args.arg2 not in VOID_ARGS) else ''
-    arg3 = '/%s' % args.arg3 if (hasattr(args, 'arg3') and args.arg3 not in VOID_ARGS) else ''
-    env = '/environment/%s' % args.environment if hasattr(args, 'environment') else ''
-    num_units = '/%s' % args.num_units if hasattr(args, 'num_units') else ''
-    return 'http://%s:%s/%s%s%s%s%s' % (args.host, args.port, args.arg1, arg2, env, arg3, num_units)
+def api_method(args, schema):
+    method = 'http://%s:%s/' % (args.host, args.port) + schema.format(**args.__dict__)
+    print(method)
+    return method
 
 if __name__ == '__main__':
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-    api = ArgumentParser(add_help=False)
-    api.add_argument('--host',     action='store', default=None, required=True)
-    api.add_argument('--port',     action='store', default=5000)
-    api.add_argument('--username', action='store', default=None)
-    api.add_argument('--password', action='store', default=None)
-    environment = ArgumentParser(add_help=False)
-    environment.add_argument('environment', action='store')
+    api_args = ArgumentParser(add_help=False)
+    api_args.add_argument('--host',     action='store', default=None, required=True)
+    api_args.add_argument('--port',     action='store', default=5000)
+    api_args.add_argument('--username', action='store', default=None)
+    api_args.add_argument('--password', action='store', default=None)
+    environment_args = ArgumentParser(add_help=False)
+    environment_args.add_argument('environment', action='store')
+    #num_units = ArgumentParser(add_help=False)
+    #num_units.add_argument('num_units', action='store')
 
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter, epilog='''TODO.''')
     subparsers = parser.add_subparsers(dest='arg1')
-    environment_parser = subparsers.add_parser('environment')
-    environment_subparsers = environment_parser.add_subparsers(dest='arg2')
-    environment_list_parser = environment_subparsers.add_parser('list', parents=[api])
-    environment_add_parser = environment_subparsers.add_parser('add', parents=[api])
-    environment_add_parser.add_argument('name', action='store')
-    environment_add_parser.add_argument('type', action='store')
-    environment_add_parser.add_argument('region', action='store')
-    environment_add_parser.add_argument('access_key', action='store')
-    environment_add_parser.add_argument('secret_key', action='store')
-    environment_add_parser.add_argument('control_bucket', action='store')
-    transform_parser = subparsers.add_parser('transform')
-    transform_subparsers = transform_parser.add_subparsers(dest='arg2')
-    transform_unit_parser = transform_subparsers.add_parser('unit')
-    transform_unit_subparsers = transform_unit_parser.add_subparsers(dest='action')
-    transform_unit_list_parser = transform_unit_subparsers.add_parser('list', parents=[api, environment])
-    transform_unit_count_parser = transform_unit_subparsers.add_parser('count', parents=[api, environment])
-    transform_unit_deploy_parser = transform_unit_subparsers.add_parser('deploy', parents=[api, environment])
-    transform_unit_deploy_parser.add_argument('num_units', action='store', default=None)
+
+    environment = subparsers.add_parser('environment')
+    environment_sub = environment.add_subparsers(dest='arg2')
+    environment_list = environment_sub.add_parser('list', parents=[api_args])
+    environment_add = environment_sub.add_parser('add', parents=[api_args])
+    environment_add.add_argument('name', action='store')
+    environment_add.add_argument('type', action='store')
+    environment_add.add_argument('region', action='store')
+    environment_add.add_argument('access_key', action='store')
+    environment_add.add_argument('secret_key', action='store')
+    environment_add.add_argument('control_bucket', action='store')
+    environment_remove = environment_sub.add_parser('remove', parents=[api_args])
+    environment_remove.add_argument('name', action='store')
+
+    transform = subparsers.add_parser('transform')
+    transform_sub = transform.add_subparsers(dest='arg2')
+    transform_unit = transform_sub.add_parser('unit')
+    transform_unit_sub = transform_unit.add_subparsers(dest='arg3')
+    transform_unit_list = transform_unit_sub.add_parser('list', parents=[api_args, environment_args])
+    transform_unit_count = transform_unit_sub.add_parser('count', parents=[api_args, environment_args])
+    transform_unit_deploy = transform_unit_sub.add_parser('deploy', parents=[api_args, environment_args])
+    transform_unit_deploy.add_argument('num_units', action='store')
+    transform_unit_remove = transform_unit_sub.add_parser('remove', parents=[api_args, environment_args])
+    transform_unit_remove.add_argument('num_units', action='store')
 
     args = parser.parse_args()
     username = args.username or 'anonymous'
     auth = (args.username, args.password) if args.username and args.password else None
     headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 
-    print(api_method(args))
-
     if args.arg1 == 'environment':
         if args.arg2 == 'list':
             print('Listing environments ...')
-            r = requests.get(api_method(args), auth=auth)
-            print(r.json())
-        if args.arg2 == 'add':
+            r = requests.get(api_method(args, '{arg1}'), auth=auth)
+            pprint(r.json())
+        elif args.arg2 == 'add':
             print('Adding a new environment %s as %s ...' % (args.name, username))
-            r = requests.post(api_method(args), data=object2json(args, False), auth=auth, headers=headers)
-            print(r.json())
+            data = object2json(args, False)
+            r = requests.post(api_method(args, '{arg1}'), data=data, auth=auth, headers=headers)
+            pprint(r.json())
+        elif args.arg2 == 'remove':
+            print('Removing environment %s as %s ...' % (args.name, username))
+            r = requests.delete(api_method(args, '{arg1}/name/{name}'), auth=auth)
+            pprint(r.json())
     elif args.arg1 == 'transform':
         if args.arg2 == 'unit':
             if args.arg3 == 'list':
                 print('Listing transform units of environment %s as %s ...' % (args.environment, username))
-                r = requests.get(api_method(args), auth=auth)
-                print(r.json())
+                r = requests.get(api_method(args, '{arg1}/{arg2}/environment/{environment}'), auth=auth)
+                pprint(r.json())
             elif args.arg3 == 'count':
                 print('Counting transform units of environment %s as %s ...' % (args.environment, username))
-                r = requests.get(api_method(args), auth=auth)
-                print(r.json())
+                r = requests.get(api_method(args, '{arg1}/{arg2}/environment/{environment}/{arg3}'), auth=auth)
+                pprint(r.json())
             elif args.arg3 == 'deploy':
-                print('Deploy %s transform units into environment %s as %s ...' % (args.num_units, args.environment, username))
-                r = requests.post(api_method(args), data=object2json({'salut': 1}, False), auth=auth, headers=headers)
-                print(r)
-                print(r.json())
+                print('Deploying %s transform units into environment %s as %s ...' %
+                      (args.num_units, args.environment, username))
+                data = object2json({'num_units': args.num_units}, False)
+                r = requests.post(api_method(args, '{arg1}/{arg2}/environment/{environment}'),
+                                  data=data, auth=auth, headers=headers)
+                pprint(r.json())
+            elif args.arg3 == 'remove':
+                print('Removing %s transform units from environment %s as %s ...' %
+                      (args.num_units, args.environment, username))
+                data = object2json({'num_units': args.num_units}, False)
+                r = requests.delete(api_method(args, '{arg1}/{arg2}/environment/{environment}'),
+                                    data=data, auth=auth, headers=headers)
+                pprint(r.json())
+
