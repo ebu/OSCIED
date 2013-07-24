b8ff421b6e74078ac3e1afaf4acfb1174dbf503a
David Fischer <david.fischer.ch@gmail.com>
2013-07-24 16:41:20 +0200

    Add assets

---
+++ charms/oscied-orchestra/media/js/fileupload/locale.js
@@ -0,0 +1,30 @@
+/*
+ * jQuery File Upload Plugin Localization Example 6.5.1
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2012, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/*global window */
+
+window.locale = {
+    "fileupload": {
+        "errors": {
+            "maxFileSize": "File too big",
+            "minFileSize": "File too small",
+            "acceptFileTypes": "File type not allowed",
+            "maxNumberOfFiles": "Too many files uploaded",
+            "uploadedBytes": "Loading error",
+            "emptyResult": "Loading error"
+        },
+        "error": "Error",
+        "start": "Load",
+        "cancel": "Cancel",
+        "delete": "Delete",
+        "destroy": "Destroy"
+    }
+};
