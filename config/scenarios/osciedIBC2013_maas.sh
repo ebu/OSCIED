5ff1a83fc0dc30fa8c52a760debe9775a44d17aa
David Fischer <david.fischer.ch@gmail.com>
2013-08-14 17:40:59 +0200

    Hardcoded config values, temporary hack

--- config/scenarios/osciedIBC2013_maas.sh
+++ config/scenarios/osciedIBC2013_maas.sh
@@ -48,68 +48,94 @@ osciedIBC2013Scenario_maas()
     juju bootstrap --environment 'maas'
   fi
 
+  pecho 'Deploy Storage (2 instance without replication)'
+  mecho "Using user define Storage configuration : $cfg_maas"
+  yesOrNo $false 'do it now'
+  if [ $REPLY -eq $true ]; then
+    juju deploy -n 2 --environment 'maas' --config "$cfg_maas" --repository=. local:$RELEASE/oscied-storage || xecho '1'
+    juju expose --environment 'maas' oscied-storage || xecho '2'
+  fi
+
+  # FIXME periodic check of juju status to wait until the storage service to be up and ready
+
   pecho 'Deploy Orchestra (1 instance)'
   mecho "Using user define Orchestra configuration : $cfg_maas"
   yesOrNo $false 'do it now'
   if [ $REPLY -eq $true ]; then
-    jitsu deploy-to 0 --environment 'maas' --config "$cfg_maas" \
-      --repository=. local:$RELEASE/oscied-orchestra orchestra || xecho '1'
-    juju expose --environment 'maas' orchestra || xecho '2'
+    jitsu deploy-to 0 --environment 'maas' --config "$cfg_maas" --repository=. \
+      local:$RELEASE/oscied-orchestra || xecho '1'
+    juju expose --environment 'maas' oscied-orchestra || xecho '2'
   fi
+
+  # FIXME periodic check of juju status to wait until the orchestra service to be up and ready
 
   pecho 'Deploy Web UI (1 instance)'
   mecho "Using user define Web UI configuration : $cfg_maas"
   yesOrNo $false 'do it now'
   if [ $REPLY -eq $true ]; then
-    jitsu deploy-to 0 --environment 'maas' --config "$cfg_maas" \
-      --repository=. local:$RELEASE/oscied-webui webui || xecho '1'
-    juju expose --environment 'maas' webui || xecho '2'
-  fi
-
-  pecho 'Deploy Storage (2 instance without replication)'
-  mecho "Using user define Storage configuration : $cfg_maas"
-  yesOrNo $false 'do it now'
-  if [ $REPLY -eq $true ]; then
-    juju deploy -n 2 --environment 'maas' --config "$cfg_maas" \
-      --repository=. local:$RELEASE/oscied-storage storage || xecho '1'
-    juju expose --environment 'maas' storage || xecho '2'
+    jitsu deploy-to 0 --environment 'maas' --config "$cfg_maas" --repository=. local:$RELEASE/oscied-webui || xecho '1'
+    juju expose --environment 'maas' oscied-webui || xecho '2'
   fi
 
   pecho 'Deploy Transform (2 instances)'
   mecho "Using user define Transform configuration : $cfg_maas"
   yesOrNo $false 'do it now'
   if [ $REPLY -eq $true ]; then
-    jitsu deploy-to 1 --environment 'maas' --config "$cfg_maas" \
-      --repository=. local:$RELEASE/oscied-transform transform1 || xecho '1'
+    jitsu deploy-to 1 --environment 'maas' --config "$cfg_maas" --repository=. \
+      local:$RELEASE/oscied-transform oscied-transform1 || xecho '1'
     jitsu deploy-to 2 --environment 'maas' --config "$cfg_maas" \
-      --repository=. local:$RELEASE/oscied-transform transform2 || xecho '2'
+      --repository=. local:$RELEASE/oscied-transform oscied-transform2 || xecho '2'
   fi
+
+  # FIXME periodic check of juju status to wait until the publication service to be up and ready
 
   pecho 'Deploy Publisher (2 instances)'
   mecho "Using user define Publisher configuration : $cfg_maas"
   yesOrNo $false 'do it now'
   if [ $REPLY -eq $true ]; then
     jitsu deploy-to 1 --environment 'maas' --config "$cfg_maas" \
-      --repository=. local:$RELEASE/oscied-publisher publisher1 || xecho '1'
+      --repository=. local:$RELEASE/oscied-publisher oscied-publisher1 || xecho '1'
     jitsu deploy-to 2 --environment 'maas' --config "$cfg_maas" \
-      --repository=. local:$RELEASE/oscied-publisher publisher2 || xecho '2'
-    juju expose --environment 'maas' publisher1 || xecho '3'
-    juju expose --environment 'maas' publisher2 || xecho '4'
+      --repository=. local:$RELEASE/oscied-publisher oscied-publisher2 || xecho '2'
+    juju expose --environment 'maas' oscied-publisher1 || xecho '3'
+    juju expose --environment 'maas' oscied-publisher2 || xecho '4'
+  fi
+
+  pecho 'Disconnect all services [DEBUG PURPOSE ONLY] (with juju remove-relation)'
+  yesOrNo $false 'do it now'
+  if [ $REPLY -eq $true ]; then
+    juju remove-relation --environment 'maas' oscied-storage oscied-orchestra
+    juju remove-relation --environment 'maas' oscied-storage oscied-webui
+    juju remove-relation --environment 'maas' oscied-storage oscied-transform1
+    juju remove-relation --environment 'maas' oscied-storage oscied-transform2
+    juju remove-relation --environment 'maas' oscied-storage oscied-publisher1
+    juju remove-relation --environment 'maas' oscied-storage oscied-publisher2
+    juju remove-relation --environment 'maas' oscied-orchestra:api oscied-webui:api
+    juju remove-relation --environment 'maas' oscied-orchestra:transform oscied-transform1:transform
+    juju remove-relation --environment 'maas' oscied-orchestra:transform oscied-transform2:transform
+    juju remove-relation --environment 'maas' oscied-orchestra:publisher oscied-publisher1:publisher
+    juju remove-relation --environment 'maas' oscied-orchestra:publisher oscied-publisher2:publisher
   fi
 
   pecho 'Connect all services together (with juju add-relation)'
   yesOrNo $false 'do it now'
   if [ $REPLY -eq $true ]; then
-    juju add-relation --environment 'maas' storage orchestra
-    juju add-relation --environment 'maas' storage webui
-    juju add-relation --environment 'maas' storage transform1
-    juju add-relation --environment 'maas' storage transform2
-    juju add-relation --environment 'maas' storage publisher1
-    juju add-relation --environment 'maas' storage publisher2
-    juju add-relation --environment 'maas' orchestra:api webui:api
-    juju add-relation --environment 'maas' orchestra:transform transform1:transform
-    juju add-relation --environment 'maas' orchestra:transform transform2:transform
-    juju add-relation --environment 'maas' orchestra:publisher publisher1:publisher
-    juju add-relation --environment 'maas' orchestra:publisher publisher2:publisher
+    juju add-relation --environment 'maas' oscied-storage oscied-orchestra
+    juju add-relation --environment 'maas' oscied-storage oscied-webui
+    juju add-relation --environment 'maas' oscied-storage oscied-transform1
+    juju add-relation --environment 'maas' oscied-storage oscied-transform2
+    juju add-relation --environment 'maas' oscied-storage oscied-publisher1
+    juju add-relation --environment 'maas' oscied-storage oscied-publisher2
+    mecho 'now this is orchestra relation with the web user interface units'
+    pause
+    juju add-relation --environment 'maas' oscied-orchestra:api oscied-webui:api
+    mecho 'now this is orchestra relation with transformation units'
+    pause
+    juju add-relation --environment 'maas' oscied-orchestra:transform oscied-transform1:transform
+    juju add-relation --environment 'maas' oscied-orchestra:transform oscied-transform2:transform
+    mecho 'now this is orchestra relation with publication units'
+    pause
+    juju add-relation --environment 'maas' oscied-orchestra:publisher oscied-publisher1:publisher
+    juju add-relation --environment 'maas' oscied-orchestra:publisher oscied-publisher2:publisher
   fi
 }
