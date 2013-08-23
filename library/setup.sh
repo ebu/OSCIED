e5c55c9a3ae02cfa653452affdccd80675a7448f
David Fischer <david.fischer.ch@gmail.com>
2013-08-23 13:17:32 +0200

    Use absolute paths

--- library/setup.sh
+++ library/setup.sh
@@ -25,13 +25,14 @@
 
 oscied_install()
 {
+  base=$(cd "$(dirname "$0")"; pwd)
   echo 'Install OSCIED Library'
   echo '1/3 - Install Python modules prerequisites'
   apt-get -y install build-essential git-core python-dev python-kitchen python-pip
   echo '2/3 - Install Python module called pyutils'
-  cd pyutils-source && ./setup.py develop || { echo 'Unable to install pyutils module' 1>&2; exit 2; }
+  cd "$base/pyutils-source" && ./setup.py develop || { echo 'Unable to install pyutils module' 1>&2; exit 2; }
   echo '3/3 - Install Python module called oscied_lib'
-  cd .. && ./setup.py develop || { echo 'Unable to install oscied_lib module' 1>&2; exit 3; }
+  cd "$base" && ./setup.py develop || { echo 'Unable to install oscied_lib module' 1>&2; exit 3; }
 }
 
 oscied_install 2>&1 > 'setup.log'
