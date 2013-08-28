11e485275797d5eb90dc87760b5f540768799c6c
David Fischer <david.fischer.ch@gmail.com>
2013-08-28 16:58:02 +0200

    Fix script to generate media list

--- scenarios/IBC2013/up-medias.sh
+++ scenarios/IBC2013/up-medias.sh
@@ -1,10 +1,12 @@
-> 'medias.csv'
-find ../../medias/EBU/ -type f | while read media
+medias="$(pwd)/medias.csv"
+cd ..
+> "$medias"
+find ../medias/EBU/ -type f | while read media
 do
   path=$(basename $(dirname "$media"))
   name=$(basename "$media")
   name=$(echo $path $name | sed 's: :_:g')
   title=$(basename "$media" | cut -d'.' -f1)
   echo "Adding $name"
-  echo "$media;$name;$title" >> 'medias.csv'
+  echo "$media;$name;$title" >> "$medias"
 done
