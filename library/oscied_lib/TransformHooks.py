5673fcc967c7e7f1332ffe0ad94d3630ec8634d0
David Fischer <david.fischer.ch@gmail.com>
2013-08-31 13:16:09 +0200

    Fix #55

--- library/oscied_lib/TransformHooks.py
+++ library/oscied_lib/TransformHooks.py
@@ -34,10 +34,19 @@ from pyutils.py_subprocess import make
 
 class TransformHooks(CharmHooks_Storage, CharmHooks_Subordinate):
 
-    PACKAGES = tuple(set(CharmHooks_Storage.PACKAGES + CharmHooks_Subordinate.PACKAGES +
-                     (u'cmake',  u'ntp')))
-    # u'libavcodec-dev', u'libavformat-dev', u'libavutil-dev',
-    # u'libswscale-dev', u'libavdevice-dev', u'libavcodec-extra-53', u'zlib1g-dev'
+    FF_BIN_PACKAGES = (u'libgnutls28', u'libass4', u'libbluray1', u'libmp3lame0', u'libvorbis0a', u'libvorbisenc2',
+                       u'libvorbisfile3', u'libvpx1', u'libxvidcore4')
+    FF_DEV_PACKAGES = (u'libgnutls28-dev', u'libass-dev', u'libbluray-dev', u'libmp3lame-dev',
+                       u'libtheora-dev', u'libvorbis-dev', u'libvpx-dev', u'libxvidcore-dev', u'zlib1g-dev')
+    FF_CPL_PACKAGES = (u'autoconf', u'automake', u'build-essential', u'checkinstall', u'libtool', u'pkg-config',
+                       u'texi2html', u'yasm', u'zlib1g-dev')
+    FF_RMV_PACKAGES = (u'libgpac2', u'libgpac-dev')
+    FF_ALL_PACKAGES = tuple(set(FF_BIN_PACKAGES + FF_DEV_PACKAGES + FF_CPL_PACKAGES))
+    FF_LIBS = (u'gnutls', u'libass', u'libbluray', u'libmp3lame', u'libvorbis', u'libvpx', u'libx264', u'libxvid')
+    FF_CONFIGURE_OPTIONS = u'--enable-gpl ' + u' '.join(u'--enable-{0}'.format(library) for library in FF_LIBS)
+
+    PACKAGES = tuple(set(CharmHooks_Storage.PACKAGES + CharmHooks_Subordinate.PACKAGES + (u'ntp',)))
+
 
     def __init__(self, metadata, default_config, local_config_filename, default_os_env):
         super(TransformHooks, self).__init__(metadata, default_config, default_os_env)
@@ -49,22 +58,45 @@ class TransformHooks(CharmHooks_Storage, CharmHooks_Subordinate):
     def hook_install(self):
         self.hook_uninstall()
         self.info(u'Upgrade system and install prerequisites')
-        self.cmd(u'apt-add-repository -y ppa:jon-severinsson/ffmpeg')
+        if 'ppa:' in self.config.ffmpeg_origin:
+            self.cmd(u'apt-add-repository -y {0}'.format(self.config.ffmpeg_origin))
         self.cmd(u'apt-get -y update', fail=False)
         self.cmd(u'apt-get -y upgrade')
         self.cmd(u'apt-get -y install {0}'.format(u' '.join(TransformHooks.PACKAGES)))
-        self.cmd(u'apt-get -y build-dep gpac x264')
         self.info(u'Restart network time protocol service')
         self.cmd(u'service ntp restart')
         # FIXME Compile and install openSVCDecoder
-        self.info(u'Compile and install openHEVC')
-        make(u'openHEVC.tar.bz2', with_cmake=True, log=self.debug)
-        self.info(u'Compile and install GPAC/DashCast')
-        make(u'gpac.tar.bz2', log=self.debug)
-        self.info(u'Compile and install x264')
-        make(u'x264.tar.bz2', configure_options=u'--enable-shared', log=self.debug)
-        # FIXME Compile and install ffmpeg
-        # http://ffmpeg.org/trac/ffmpeg/wiki/UbuntuCompilationGuide
+        if 'tar.bz2' in self.config.open_hevc_origin:
+            self.info(u'Compile and install openHEVC from archive {0}'.format(self.config.open_hevc_origin))
+            self.cmd(u'apt-get -y install cmake')
+            self.debug(make(self.config.open_hevc_origin, with_cmake=True, log=self.debug)['cmake'])
+        if 'tar.bz2' in self.config.x264_origin:
+            self.info(u'Compile and install x264')
+            self.cmd(u'apt-get -y build-dep x264')
+            self.debug(make(self.config.x264_origin, configure_options=u'--enable-shared', log=self.debug)['configure'])
+        else:
+            self.info(u'Install x264')
+            self.cmd(u'apt-get -y install x264')
+        if 'tar.bz2' in self.config.ffmpeg_origin:
+            self.info(u'Compile and install ffmpeg from archive {0}'.format(self.config.ffmpeg_origin))
+            self.cmd(u'apt-get -y install {0}'.format(u' '.join(TransformHooks.FF_ALL_PACKAGES)))
+            self.cmd(u'apt-get -y remove {0}'.format(u' '.join(TransformHooks.FF_RMV_PACKAGES)))
+            self.debug(make(self.config.ffmpeg_origin, configure_options=TransformHooks.FF_CONFIGURE_OPTIONS,
+                       log=self.debug)['configure'])
+        else:
+            self.info(u'Install ffmpeg')
+            self.cmd(u'apt-get -y install ffmpeg')
+        if 'tar.bz2' in self.config.gpac_origin:
+            self.info(u'Compile and install GPAC/DashCast from archive {0}'.format(self.config.gpac_origin))
+            self.cmd(u'apt-get -y build-dep gpac')
+            self.debug(make(u'gpac.tar.bz2', log=self.debug)['configure'])
+        elif 'deb' in self.config.gpac_origin:
+            self.info(u'Install GPAC/DashCast from debian package {0}'.format(self.config.gpac_origin))
+            self.cmd(u'dpkg -i {0}'.format(u'gpac_0.5.1.DEV-r4722_i386.deb'), fail=False)
+            self.cmd(u'apt-get -y -f install')
+        else:
+            self.info(u'Install GPAC/DashCast')
+            self.cmd(u'apt-get -y install gpac')
 
     def hook_config_changed(self):
         self.storage_remount()
