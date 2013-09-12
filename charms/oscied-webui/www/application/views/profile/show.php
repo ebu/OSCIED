c461d2b3c331e544b779ee06465fb0f3d3604339
David Fischer <david.fischer.ch@gmail.com>
2013-09-13 01:42:02 +0200

    Put forms on top

--- charms/oscied-webui/www/application/views/profile/show.php
+++ charms/oscied-webui/www/application/views/profile/show.php
@@ -1,5 +1,9 @@
 <h1 class="page-header">Available transform profiles</h1>
 
+<?php if ($this->user->is_logged()): ?>
+<?= $this->load->view('profile/add_profile_form'); ?>
+<?php endif; ?>
+
 <div id="profiles">
    <?= $this->load->view('profile/show_profiles') ?>
 </div>
@@ -19,7 +23,3 @@ $('body').on('click', '.delete', function () {
    return false;
 });
 </script>
-
-<?php if ($this->user->is_logged()): ?>
-<?= $this->load->view('profile/add_profile_form'); ?>
-<?php endif; ?>
