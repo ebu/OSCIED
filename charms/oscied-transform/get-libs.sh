6e3a9ecece62781c84a618a56212a447cea14478
David Fischer <david.fischer.ch@gmail.com>
2013-12-18 16:16:55 +0100

    Remove FFmbc + do not load too much encoders (still need fix for #106+#130)

--- charms/oscied-transform/get-libs.sh
+++ charms/oscied-transform/get-libs.sh
@@ -17,11 +17,11 @@ get_from_svn()
   rm -rf $1
 }
 
-get_from_svn 'gpac'           'http://svn.code.sf.net/p/gpac/code/trunk/gpac/' 1
-get_from_git 'ffmpeg'         'git://source.ffmpeg.org/ffmpeg.git'             2
-get_from_svn 'openSVCDecoder' 'svn://svn.code.sf.net/p/opensvcdecoder/code/'   3
-get_from_git 'openHEVC'       'git://github.com/OpenHEVC/openHEVC.git'         4
-get_from_git 'x264'           'git://git.videolan.org/x264.git'                5
+#get_from_svn 'gpac'           'http://svn.code.sf.net/p/gpac/code/trunk/gpac/' 1
+#get_from_git 'ffmpeg'         'git://source.ffmpeg.org/ffmpeg.git'             2
+#get_from_svn 'openSVCDecoder' 'svn://svn.code.sf.net/p/opensvcdecoder/code/'   3
+#get_from_git 'openHEVC'       'git://github.com/OpenHEVC/openHEVC.git'         4
+#get_from_git 'x264'           'git://git.videolan.org/x264.git'                5
 
-wget -N http://download.tsi.telecom-paristech.fr/gpac/latest_builds/linux64/gpac/gpac_0.5.1.DEV-r4939_amd64.deb
-wget -N http://download.tsi.telecom-paristech.fr/gpac/latest_builds/linux32/gpac/gpac_0.5.1.DEV-r4939_i386.deb
+wget -N http://download.tsi.telecom-paristech.fr/gpac/latest_builds/linux64/gpac/gpac_0.5.1.DEV-r4944_amd64.deb
+#wget -N http://download.tsi.telecom-paristech.fr/gpac/latest_builds/linux32/gpac/gpac_0.5.1.DEV-r4944_i386.deb
